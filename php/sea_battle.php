b15bb59d1bb24a41ae5f41fd00560cc1ce435da2
Grigoriy Guskov <guskov.ga@phystech.edu>
2023-10-17 16:52:34 +0300

    added some usefull functions

--- php/sea_battle.php
+++ php/sea_battle.php
@@ -1,26 +1,123 @@
 <?php
 
 define ("MATRIX_SIZE", 10);
-//проверка можно ли поставить корабль
-function is_empty($matrix, $x, $y, $dir, $lenght) {
-	if($dir == 1) {
-		if($x > MATRIX_SIZE - $lenght)
-			return 0;
-		for($i = 0; $i < $lenght; ++$i) {
-			if($matrix[$y][$x + $i])
-				return 0;
+
+class Ship {
+	public $x;
+	public $y;
+	public $dir;
+	public $lenght;
+	public $alive = 1;
+
+	public function is_dead($matrix) {
+		for($i = 0; $i < .$lenght; ++$i) {
+			if(.$dir == 0) {
+				if($matrix[.$y + $i][.$x] == .$lenght) {
+					.$alive = 1;
+					return 0;
+				} else if($matrix[.$y + $i][.$x] != .$lenght + 4)
+					exit();
+			} else {
+				if($matrix[.$y][.$x + $i] == .$lenght) {
+					.$alive = 1;
+					return 0;
+				} else if($matrix[.$y][.$x + $i] != .$lenght + 4)
+					exit();
+			}
 		}
-	} 
-	if($dir == 0) {
-		if($y > MATRIX_SIZE - $lenght)
-			return 0;
-		for($i = 0; $i < $lenght; ++$i) {
-			if($matrix[$y + $i][$x])
+		.$alive = 0;
+		return 1;
+	}
+	//проверка можно ли поставить корабль
+	public function is_empty($matrix) {
+		if(.$dir == 1) {
+			if(.$x > MATRIX_SIZE - .$lenght)
 				return 0;
+			for($i = 0; $i < .$lenght; ++$i) {
+				if($matrix[.$y][.$x + $i])
+					return 0;
+			}
+		} 
+		if(.$dir == 0) {
+			if(.$y > MATRIX_SIZE - .$lenght)
+				return 0;
+			for($i = 0; $i < .$lenght; ++$i) {
+				if($matrix[.$y + $i][.$x])
+					return 0;
+			}
+		}
+		return 1;
+	}
+	public function place_ship($matrix) {
+		//окружаем корабль "забором"
+		if(.$dir == 0) {
+			if(.$x > 0) {
+				$matrix[.$y][.$x-1] = -1;
+				if(.$y > 0) 
+					$matrix[.$y-1].[$x-1] = -1;
+				if(.$y < 9)
+					$matrix[.$y+1][.$x-1] = -1;
+			}
+			if(.$x+.$lenght-1 < 9) {
+				$matrix[.$y][.$x+.$lenght] = -1;
+				if(.$y > 0) 
+					$matrix[.$y-1][.$x+.$lenght] = -1;
+				if(.$y < 9)
+					$matrix[.$y+1][.$x+.$lenght] = -1;
+			}
+		}
+		else {
+			if(.$y > 0) {
+				$matrix[.$y-1][.$x] = -1;
+				if(.$x > 0)
+					$matrix[.$y-1][.$x-1] = -1;
+				if(.$x < 9)
+					$matrix[.$y-1][.$x+1] = -1;
+			}
+			if(.$y+.$lenght-1 < 9) {
+				$matrix[.$y+.$lenght][.$x] = -1;
+				if($x > 0)
+					$matrix[.$y+.$lenght][.$x-1] = -1;
+				if(.$x < 9)
+					$matrix[.$y+.$lenght][.$x+1] = -1;
+			}
+		}
+		//ставим сам корабль
+		for($j = 0; $j < .$lenght; ++$j) {
+			if(.$dir == 0) {
+				$matrix[.$y + $j][.$x] = .$lenght;
+				if(.$x < 9)
+					$matrix[.$y + $j][.$x+1] = -1;
+				if(.$x > 0)
+					$matrix[.$y + $j][.$x-1] = -1;
+			} else {
+				$matrix[.$y][.$x + $j] = .$lenght;
+				if(.$y < 9)
+					$matrix[.$y+1][.$x + $j] = -1;
+				if(.$y > 0)
+					$matrix[.$y-1][.$x + $j] = -1;
+			}
 		}
 	}
-	return 1;
 }
+
+
+//функция для выстрела
+function shot($matrix, $x, $y) {
+	if($matrix[$y][$x] <= 0) {
+		$matrix[$y][$x] = -2;
+		return 0;
+	}
+	if($matrix[$y][$x] / 5 == 0) {
+		$matrix[$y][$x] += 4;
+		return 1;
+	}
+	return -1; 
+
+
+}
+
+
 //создаем поле
 for ($i=0; $i < MATRIX_SIZE; ++$i) {
 	for ($j=0; $j < MATRIX_SIZE; ++$j) {
@@ -28,62 +125,19 @@ for ($i=0; $i < MATRIX_SIZE; ++$i) {
 	}
 }
 //расставляем корабли
-for($i = 4; $i > 3; --$i) {
-	$empt_fl = 0;
-	//задаем координаты
-	for(; !$empt_fl; $empt_fl = is_empty($mat, $x, $y, $dir, $i)) {
-		$x = rand(0, 10);
-		$y = rand(0, 10);
-		$dir = rand(0, 1);
-	}
-	//окружаем корабль "забором"
-	if($dir == 0) {
-		if($x > 0) {
-			$mat[$y][$x-1] = -1;
-			if($y > 0) 
-				$mat[$y-1][$x-1] = -1;
-			if($y < 9)
-				$mat[$y+1][$x-1] = -1;
+for($shcount = 0, $i = 4; $i > 3; --$i) {
+	for($k = 0; $k < $i - 4; ++$k, ++$shcount) {
+		//задаем координаты
+		for($empt_fl = 0; !$empt_fl;) {
+			$ships[$shcount] = new Ship;
+			$ships[$shcount]->$x = rand(0, 10);
+			$ships[$shcount]->$y = rand(0, 10);
+			$ships[$shcount]->$dir = rand(0, 1);
+			$ships[$shcount]->$lenght = $i;
+			$empt_fl = $ships[$shcount].is_empty($mat);
 		}
-		if($x+$i-1 < 9) {
-			$mat[$y][$x+$i] = -1;
-			if($y > 0) 
-				$mat[$y-1][$x+$i] = -1;
-			if($y < 9)
-				$mat[$y+1][$x+$i] = -1;
-		}
-	}
-	else {
-		if($y > 0) {
-			$mat[$y-1][$x] = -1;
-			if($x > 0)
-				$mat[$y-1][$x-1] = -1;
-			if($x < 9)
-				$mat[$y-1][$x+1] = -1;
-		}
-		if($y+$i-1 < 9) {
-			$mat[$y+$i][$x] = -1;
-			if($x > 0)
-				$mat[$y+$i][$x-1] = -1;
-			if($x < 9)
-				$mat[$y+$i][$x+1] = -1;
-		}
-	}
-	//ставим сам корабль
-	for($j = 0; $j < $i; ++$j) {
-		if($dir == 0) {
-			$mat[$y + $j][$x] = $i;
-			if($x < 9)
-				$mat[$y + $j][$x+1] = -1;
-			if($x > 0)
-				$mat[$y + $j][$x-1] = -1;
-		} else {
-			$mat[$y][$x + $j] = $i;
-			if($y < 9)
-				$mat[$y+1][$x + $j] = -1;
-			if($y > 0)
-				$mat[$y-1][$x + $j] = -1;
-		}
+		//ставим корабль на поле
+		$ships[$shcount].place_ship($mat);
 	}
 }
 //печатаем поле
