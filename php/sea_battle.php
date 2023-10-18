96cf118d70379238eec46f5140b3c1c27301d120
Grigoriy Guskov <guskov.ga@phystech.edu>
2023-10-18 17:46:18 +0300

    working version

--- php/sea_battle.php
+++ php/sea_battle.php
@@ -3,166 +3,166 @@
 define ("MATRIX_SIZE", 10);
 
 class Ship {
-	public $x;
-	public $y;
-	public $dir;
-	public $lenght;
+	public $x = -1;
+	public $y= -1;
+	public $dir = -1;
+	public $lenght = -1;
 	public $alive = 1;
-
-	public function is_dead($matrix) {
-		for($i = 0; $i < $this->$lenght; ++$i) {
-			if($this->$dir == 0) {
-				if($matrix[$this->$y + $i][$this->$x] == $this->$lenght) {
-					$this->$alive = 1;
+	
+	public function is_dead(&$matrix) {
+		for($i = 0; $i < $this->lenght; ++$i) {
+			if($this->dir == 0) {
+				if($matrix[$this->y + $i][$this->x] == $this->lenght) {
+					$this->alive = 1;
 					return 0;
-				} else if($matrix[$this->$y + $i][$this->$x] != $this->$lenght + 4)
+				} else if($matrix[$this->y + $i][$this->x] != $this->lenght + 4)
 					exit();
 			} else {
-				if($matrix[$this->$y][$this->$x + $i] == $this->$lenght) {
-					$this->$alive = 1;
+				if($matrix[$this->y][$this->x + $i] == $this->lenght) {
+					$this->alive = 1;
 					return 0;
-				} else if($matrix[$this->$y][$this->$x + $i] != $this->$lenght + 4)
+				} else if($matrix[$this->y][$this->x + $i] != $this->lenght + 4)
 					exit();
 			}
 		}
-		$this->$alive = 0;
+		$this->alive = 0;
 		return 1;
 	}
 	//проверка можно ли поставить корабль
-	public function is_empty($matrix) {
-		if($this->$dir == 1) {
-			if($this->$x > MATRIX_SIZE - $this->$lenght)
+	public function is_empty(&$matrix) {
+		if($this->dir == 1) {
+			if($this->x > MATRIX_SIZE - $this->lenght)
 				return 0;
-			for($i = 0; $i < $this->$lenght; ++$i) {
-				if($matrix[$this->$y][$this->$x + $i])
+			for($i = 0; $i < $this->lenght; ++$i) {
+				if($matrix[$this->y][$this->x + $i])
 					return 0;
 			}
 		} 
-		if($this->$dir == 0) {
-			if($this->$y > MATRIX_SIZE - $this->$lenght)
+		if($this->dir == 0) {
+			if($this->y > MATRIX_SIZE - $this->lenght)
 				return 0;
-			for($i = 0; $i < $this->$lenght; ++$i) {
-				if($matrix[$this->$y + $i][$this->$x])
+			for($i = 0; $i < $this->lenght; ++$i) {
+				if($matrix[$this->y + $i][$this->x])
 					return 0;
 			}
 		}
 		return 1;
 	}
-	public function place_ship($matrix) {
+	public function place_ship(&$matrix) {
 		if($this->is_empty($matrix)) {
 			//окружаем корабль "забором"
-			if($this->$dir == 0) {
-				if($this->$x > 0) {
-					$matrix[$this->$y][$this->$x-1] = -1;
-					if($this->$y > 0) 
-						$matrix[$this->$y-1][$this->$x-1] = -1;
-					if($this->$y < 9)
-						$matrix[$this->$y+1][$this->$x-1] = -1;
+			if($this->dir == 1) {
+				if($this->x > 0) {
+					$matrix[$this->y][$this->x-1] = -1;
+					if($this->y > 0) 
+						$matrix[$this->y-1][$this->x-1] = -1;
+					if($this->y < 9)
+						$matrix[$this->y+1][$this->x-1] = -1;
 				}
-				if($this->$x+$this->$lenght-1 < 9) {
-					$matrix[$this->$y][$this->$x+$this->$lenght] = -1;
-					if($this->$y > 0) 
-						$matrix[$this->$y-1][$this->$x+$this->$lenght] = -1;
-					if($this->$y < 9)
-						$matrix[$this->$y+1][$this->$x+$this->$lenght] = -1;
+				if($this->x+$this->lenght-1 < 9) {
+					$matrix[$this->y][$this->x+$this->lenght] = -1;
+					if($this->y > 0) 
+						$matrix[$this->y-1][$this->x+$this->lenght] = -1;
+					if($this->y < 9)
+						$matrix[$this->y+1][$this->x+$this->lenght] = -1;
 				}
 			}
 			else {
-				if($this->$y > 0) {
-					$matrix[$this->$y-1][$this->$x] = -1;
-					if($this->$x > 0)
-						$matrix[$this->$y-1][$this->$x-1] = -1;
-					if($this->$x < 9)
-						$matrix[$this->$y-1][$this->$x+1] = -1;
+				if($this->y > 0) {
+					$matrix[$this->y-1][$this->x] = -1;
+					if($this->x > 0)
+						$matrix[$this->y-1][$this->x-1] = -1;
+					if($this->x < 9)
+						$matrix[$this->y-1][$this->x+1] = -1;
 				}
-				if($this->$y+$this->$lenght-1 < 9) {
-					$matrix[$this->$y+$this->$lenght][$this->$x] = -1;
-					if($x > 0)
-						$matrix[$this->$y+$this->$lenght][$this->$x-1] = -1;
-					if($this->$x < 9)
-						$matrix[$this->$y+$this->$lenght][$this->$x+1] = -1;
+				if($this->y+$this->lenght-1 < 9) {
+					$matrix[$this->y+$this->lenght][$this->x] = -1;
+					if($this->x > 0)
+						$matrix[$this->y+$this->lenght][$this->x-1] = -1;
+					if($this->x < 9)
+						$matrix[$this->y+$this->lenght][$this->x+1] = -1;
 				}
 			}
 			//ставим сам корабль
-			for($j = 0; $j < $this->$lenght; ++$j) {
-				if($this->$dir == 0) {
-					$matrix[$this->$y + $j][$this->$x] = $this->$lenght;
-					if($this->$x < 9)
-						$matrix[$this->$y + $j][$this->$x+1] = -1;
-					if($this->$x > 0)
-						$matrix[$this->$y + $j][$this->$x-1] = -1;
+			for($j = 0; $j < $this->lenght; ++$j) {
+				if($this->dir == 0) {
+					$matrix[$this->y + $j][$this->x] = $this->lenght;
+					if($this->x < 9)
+						$matrix[$this->y + $j][$this->x+1] = -1;
+					if($this->x > 0)
+						$matrix[$this->y + $j][$this->x-1] = -1;
 				} else {
-					$matrix[$this->$y][$this->$x + $j] = $this->$lenght;
-					if($this->$y < 9)
-						$matrix[$this->$y+1][$this->$x + $j] = -1;
-					if($this->$y > 0)
-						$matrix[$this->$y-1][$this->$x + $j] = -1;
+					$matrix[$this->y][$this->x + $j] = $this->lenght;
+					if($this->y < 9)
+						$matrix[$this->y+1][$this->x + $j] = -1;
+					if($this->y > 0)
+						$matrix[$this->y-1][$this->x + $j] = -1;
 				}
 			}
 		}
 	}
-	public function is_that_ship($matrix, $x, $y) {
+	public function is_that_ship(&$matrix, $x, $y) {
 		$value = $matrix[$x][$y];
 		if($value > 4)
 			$value -= 4;
-		if($value != $this->$lenght)
+		if($value != $this->lenght)
 			return 0;
 
-		if($this->$dir == 0) {
-			if($x != $this->$x || $y < $this->$y || $y > $this->$y + $this->$lenght - 1)
+		if($this->dir == 0) {
+			if($x != $this->x || $y < $this->y || $y > $this->y + $this->lenght - 1)
 				return 0;
 		} else {
-			if($y != $this->$y || $x < $this->$x || $x > $this->$x + $this->$lenght - 1)
+			if($y != $this->y || $x < $this->x || $x > $this->x + $this->lenght - 1)
 				return 0;
 		}
 		return 1;
 
 	}
-	public function death($matrix) {
+	public function death(&$matrix) {
 		if(!$alive && $this->is_dead($matrix)) {
-			if($this->$dir == 0) {
-				if($this->$x > 0) {
-					$matrix[$this->$y][$this->$x-1] = -2;
-					if($this->$y > 0) 
-						$matrix[$this->$y-1][$this->$x-1] = -2;
-					if($this->$y < 9)
-						$matrix[$this->$y+1][$this->$x-1] = -2;
+			if($this->dir == 0) {
+				if($this->x > 0) {
+					$matrix[$this->y][$this->x-1] = -2;
+					if($this->y > 0) 
+						$matrix[$this->y-1][$this->x-1] = -2;
+					if($this->y < 9)
+						$matrix[$this->y+1][$this->x-1] = -2;
 				}
-				if($this->$x+$this->$lenght-1 < 9) {
-					$matrix[$this->$y][$this->$x+$this->$lenght] = -2;
-					if($this->$y > 0) 
-						$matrix[$this->$y-1][$this->$x+$this->$lenght] = -2;
-					if($this->$y < 9)
-						$matrix[$this->$y+1][$this->$x+$this->$lenght] = -2;
+				if($this->x+$this->lenght-1 < 9) {
+					$matrix[$this->y][$this->x+$this->lenght] = -2;
+					if($this->y > 0) 
+						$matrix[$this->y-1][$this->x+$this->lenght] = -2;
+					if($this->y < 9)
+						$matrix[$this->y+1][$this->x+$this->lenght] = -2;
 				}
 			}
 			else {
-				if($this->$y > 0) {
-					$matrix[$this->$y-1][$this->$x] = -2;
-					if($this->$x > 0)
-						$matrix[$this->$y-1][$this->$x-1] = -2;
-					if($this->$x < 9)
-						$matrix[$this->$y-1][$this->$x+1] = -2;
+				if($this->y > 0) {
+					$matrix[$this->y-1][$this->x] = -2;
+					if($this->x > 0)
+						$matrix[$this->y-1][$this->x-1] = -2;
+					if($this->x < 9)
+						$matrix[$this->y-1][$this->x+1] = -2;
 				}
-				if($this->$y+$this->$lenght-1 < 9) {
-					$matrix[$this->$y+$this->$lenght][$this->$x] = -2;
+				if($this->y+$this->lenght-1 < 9) {
+					$matrix[$this->y+$this->lenght][$this->x] = -2;
 					if($x > 0)
-						$matrix[$this->$y+$this->$lenght][$this->$x-1] = -2;
-					if($this->$x < 9)
-						$matrix[$this->$y+$this->$lenght][$this->$x+1] = -2;
+						$matrix[$this->y+$this->lenght][$this->x-1] = -2;
+					if($this->x < 9)
+						$matrix[$this->y+$this->lenght][$this->x+1] = -2;
 				}
 			}
-			for($j = 0; $j < $this->$lenght; ++$j) {
-				if($this->$dir == 0) {
-					if($this->$x < 9)
-						$matrix[$this->$y + $j][$this->$x+1] = -2;
-					if($this->$x > 0)
-						$matrix[$this->$y + $j][$this->$x-1] = -2;
+			for($j = 0; $j < $this->lenght; ++$j) {
+				if($this->dir == 0) {
+					if($this->x < 9)
+						$matrix[$this->y + $j][$this->x+1] = -2;
+					if($this->x > 0)
+						$matrix[$this->y + $j][$this->x-1] = -2;
 				} else {
-					if($this->$y < 9)
-						$matrix[$this->$y+1][$this->$x + $j] = -2;
-					if($this->$y > 0)
-						$matrix[$this->$y-1][$this->$x + $j] = -2;
+					if($this->y < 9)
+						$matrix[$this->y+1][$this->x + $j] = -2;
+					if($this->y > 0)
+						$matrix[$this->y-1][$this->x + $j] = -2;
 				}
 			}
 		}
@@ -171,7 +171,7 @@ class Ship {
 
 
 //функция для выстрела
-function shot($matrix, $x, $y) {
+function shot(&$matrix, $x, $y) {
 	if($matrix[$y][$x] < -1 || $matrix[$y][$x] > 4)
 		return -1;
 	if($matrix[$y][$x] <= 0) {
@@ -192,14 +192,14 @@ for ($i=0; $i < MATRIX_SIZE; ++$i) {
 }
 //расставляем корабли
 for($shcount = 0, $i = 4; $i > 0; --$i) {
-	for($k = 0; $k < $i - 4; ++$k, ++$shcount) {
+	for($k = 0; $k < 5 - $i; ++$k, ++$shcount) {
 		//задаем координаты
 		$ships[$shcount] = new Ship;
 		for($empt_fl = 0; !$empt_fl;) {
-			$ships[$shcount]->$x = rand(0, 10);
-			$ships[$shcount]->$y = rand(0, 10);
-			$ships[$shcount]->$dir = rand(0, 1);
-			$ships[$shcount]->$lenght = $i;
+			$ships[$shcount]->x = rand(0, 9);
+			$ships[$shcount]->y = rand(0, 9);
+			$ships[$shcount]->dir = rand(0, 1);
+			$ships[$shcount]->lenght = $i;
 			$empt_fl = $ships[$shcount]->is_empty($mat);
 		}
 		//ставим корабль на поле
@@ -208,12 +208,33 @@ for($shcount = 0, $i = 4; $i > 0; --$i) {
 }
 //печатаем поле
 print "<table border=\"1\" > <tr>";
-	for ($i=0; $i < MATRIX_SIZE; ++$i) {
-		for ($j=0; $j < MATRIX_SIZE; ++$j) {
-			print "<td width = \"50\" height = \"50\" > ".$mat[$i][$j]."</td>";
+print "<td bgcolor = black width = \"50\" height = \"50\" > </td>";
+for($i = 0, $let = "ABCDEFGHIJ"; $i < MATRIX_SIZE; ++$i) {
+	print "<td bgcolor = orange width = \"50\" height = \"50\" > ". $let[$i]."</td>";
+}
+print "</tr>";
+for ($i=0; $i < MATRIX_SIZE; ++$i) {
+	print "<td bgcolor = orange width = \"50\" height = \"50\" >".$i."</td>";
+	for ($j=0; $j < MATRIX_SIZE; ++$j) {
+		switch($mat[$i][$j]) {
+			case 4:
+				print "<td bgcolor = blue width = \"50\" height = \"50\" > ".$mat[$i][$j]."</td>";
+				break;
+			case 3:
+				print "<td bgcolor = green width = \"50\" height = \"50\" > ".$mat[$i][$j]."</td>";
+				break;
+			case 2:
+				print "<td bgcolor = purple width = \"50\" height = \"50\" > ".$mat[$i][$j]."</td>";
+				break;
+			case 1:
+				print "<td bgcolor = pink width = \"50\" height = \"50\" > ".$mat[$i][$j]."</td>";
+				break;
+			default:
+				print "<td bgcolor = yellow width = \"50\" height = \"50\" > ".$mat[$i][$j]."</td>";
 		}
-		print "</tr>";
 	}
+	print "</tr>";
+}
 print "</table>";
 
 ?>
